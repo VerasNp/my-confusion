24c916ff1fe946339d964a39428c2a18bee03bfc
VerasNp <vitorveras01@gmail.com>
2022-07-24 16:09:11 -0300

    Adds c# init

---
+++ docs/dot-net/LearnCSharp/WorkingWithData/introduction.md
@@ -0,0 +1,12 @@
+# Introduction
+
+## A brief introduction about "data"
+
+Data is a value stored in the computer's memory. It's possible to represent 256 different combinations with just 8 bits.
+The number `45` is compatible to `101101` (base-2 or binary).
+
+| 128 | 64  | 32  | 16  | 8   | 4   | 2   | 1   |
+|-----|-----|-----|-----|-----|-----|-----|-----|
+| 0   | 0   | 1   | 0   | 1   | 1   | 0   | 1   |
+**_NOTE_**: `32 + 8 + 4 + 1 = 45`
+
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: '私の混乱',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
@@ -21,7 +21,7 @@ const config = {
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
+  // to replace "en" with "Jekyllzh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
